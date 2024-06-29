f194a0ade8054b0442bcbe58b0694f2dd45c4a1f
rgap <r.guzmanap@gmail.com>
2024-06-29 01:30:24 -0500

    fix: imports fixed

--- src/app/(main-layout-pages)/(profile-module)/email-verification/page.js
+++ src/app/(main-layout-pages)/(profile-module)/email-verification/page.js
@@ -1,5 +1,5 @@
 "use client";
-import sendGetRequest from "@/services/sendGetRequest";
+import { sendGetRequest } from "@/services";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useCallback, useEffect, useRef, useState } from "react";
